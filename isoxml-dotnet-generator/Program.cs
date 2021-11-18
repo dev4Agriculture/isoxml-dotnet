451efedf476eecf0764c75265121b04d1c1f5c7f
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2021-11-18 09:48:31 +0100

    +ADD: Code Generator -DEL: Remove duplicated classes Common and External

---
+++ isoxml-dotnet-generator/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace isoxml_dotnet_generator
+{
+    class Program
+    {
+        private static void ValidationCallBack(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("\tWarning: Matching schema not found.  No validation occurred." + args.Message);
+            else
+                Console.WriteLine("\tValidation error: " + args.Message);
+        }
+
+        static void Main(string[] args)
+        {
+            string folder = String.Join(" ",args).Trim();
+            string xsdString = File.ReadAllText(folder + "ISO11783_TaskFile_V4-3.xsd");
+            TextReader textReader = new StringReader(xsdString);
+            XmlSchema xmlSchema = XmlSchema.Read(textReader, ValidationCallBack);
+            XmlSchemas xmlSchemas = new XmlSchemas();
+            xmlSchemas.Add(xmlSchema);
+            XmlSchemaImporter xmlSchemaImporter = new XmlSchemaImporter(xmlSchemas);
+            /*xmlSchemaImporter.
+            CodeDOM codeDom = new CodeDom();   
+            File.WriteAllText(folder + "ISO11783_TaskFile.cs",formattedCode);*/
+        }
+
+    }
+}
--- isoxml_dotnet/generate-xsd.sh
+++ isoxml_dotnet/generate-xsd.sh
@@ -10,15 +10,11 @@ xsltproc resources/xslt/addAnnotations.xsl resources/xsd_original/ISO11783_LinkL
 rm -rf ./src/xml/
 
 mkdir ./src/xml/
-xsd ./resources/xsd_converted/ISO11783_Common_V4-3.xsd  -c -namespace:de.dev4agriculture.iso11783.common -out:src/xml
 xsd ./resources/xsd_converted/ISO11783_TaskFile_V4-3.xsd -c -namespace:de.dev4agriculture.iso11783.taskdata -out:src/xml
-xsd ./resources/xsd_converted/ISO11783_ExternalFile_V4-3.xsd -c -namespace:de.dev4agriculture.iso11783.external -out:src/xml
 xsd ./resources/xsd_converted/ISO11783_TimeLog_V4-3.xsd -c -namespace:de.dev4agriculture.iso11783.timelog -out:src/xml
 xsd ./resources/xsd_converted/ISO11783_LinkListFile_V4-3.xsd -c -namespace:de.dev4agriculture.iso11783.linklist -out:src/xml
 
-mv ./src/xml/ISO11783_Common_V4-3.cs ./src/xml/ISO11783_Common.cs
 mv ./src/xml/ISO11783_TaskFile_V4-3.cs ./src/xml/ISO11783_TaskFile.cs
-mv ./src/xml/ISO11783_ExternalFile_V4-3.cs ./src/xml/ISO11783_ExternalFile.cs
 mv ./src/xml/ISO11783_TimeLog_V4-3.cs ./src/xml/ISO11783_TimeLog.cs
 mv ./src/xml/ISO11783_LinkListFile_V4-3.cs ./src/xml/ISO11783_LinkListFile.cs
 
