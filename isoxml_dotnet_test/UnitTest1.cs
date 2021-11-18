ddda99a6bf234e312e2e2bc5b6baec34b0b56f65
Alexander Parshin <parshin.alexander@gmail.com>
2021-11-18 12:48:21 +0300

    Copy test files to output folder

--- isoxml_dotnet_test/UnitTest1.cs
+++ isoxml_dotnet_test/UnitTest1.cs
@@ -1,10 +1,7 @@
-using de.dev4agriculture.core.iso11783;
 using de.dev4agriculture.core.iso11783.taskdata;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
-using System.Text;
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace isoxml_dotnet_test
@@ -17,7 +14,7 @@ namespace isoxml_dotnet_test
         {
             //var dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\Names.txt"); +"/testdata/";
             XmlSerializer serializer = new XmlSerializer(typeof(ISO11783_TaskData));
-            string text = File.ReadAllText("C:/src/dev4Agriculture/isoxml-dotnet/isoxml_dotnet_test/testdata/devices/TASKDATA_Devices.XML");
+            string text = File.ReadAllText("./testdata/devices/TASKDATA_Devices.XML");
             using ( StringReader reader = new StringReader(text))
             {
                 var output = (ISO11783_TaskData)serializer.Deserialize(reader);
