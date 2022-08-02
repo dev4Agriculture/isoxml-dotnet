09c29dda5e843add5266d9d9e19051d4b66a6569
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-08-02 23:30:16 +0200

    =OPT: Update XmlSchemaGenerator nuget Package +ADD: Import External Files -FIX: Some Attributes in Point were not generated properly due to the xs:union with emptyString =OPT: Renamed Parser.cs to IsoxmlParser.cs to match Class Name +ADD: Make ResultType in ResultMessage an Enum for simple analysis +ADD: Copy generated Classes to Core-Folder

---
+++ isoxml_dotnet_test/ExternalFileContentsTests.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dev4ag;
+using System;
+
+namespace isoxml_dotnet_test
+{
+    [TestClass]
+    public class ExternalFileContentsTests
+    {
+        [TestMethod]
+        public void SingleTaskDataIsParsed()
+        {
+            string path = "./testdata/ExternalFiles/NoExternals/TASKDATA.XML";
+            var result = ISOXMLParser.Load(path);
+
+            result.messages.ForEach(msg => {
+                Console.WriteLine(msg.title);
+            });
+            Assert.IsNotNull(result.result);
+            Assert.AreEqual(0, result.messages.Count);
+
+        }
+
+        [TestMethod]
+        public void TaskDataWithSingleExternalFileIsParsed()
+        {
+            string path = "./testdata/ExternalFiles/OneExternal/TASKDATA.XML";
+            var result = ISOXMLParser.Load(path);
+
+            result.messages.ForEach(msg => {
+                Console.WriteLine(msg.title);
+            });
+            Assert.IsNotNull(result.result);
+            Assert.AreEqual(result.result.Customer.Count, 3);
+            Assert.AreEqual(result.result.Task.Count, 3);
+            Assert.AreEqual(0, result.messages.Count);
+
+        }
+
+        [TestMethod]
+        public void TaskDataWithMultipleExternalFilesIsParsed()
+        {
+            string path = "./testdata/ExternalFiles/MultipleExternals/TASKDATA.XML";
+            var result = ISOXMLParser.Load(path);
+
+            result.messages.ForEach(msg => {
+                Console.WriteLine(msg.title);
+            });
+            Assert.IsNotNull(result.result);
+            Assert.AreEqual(result.result.Farm.Count, 2);
+            Assert.AreEqual(result.result.Customer.Count, 3);
+            Assert.AreEqual(result.result.Task.Count, 3);
+            Assert.AreEqual(0, result.messages.Count);
+        }
+
+
+
+        [TestMethod]
+        public void TaskDataWithMissingExternalFilesIsRecognized()
+        {
+            string path = "./testdata/ExternalFiles/MissingExternals/TASKDATA.XML";
+            var result = ISOXMLParser.Load(path);
+
+            result.messages.ForEach(msg => {
+                Console.WriteLine(msg.title);
+            });
+            Assert.AreNotEqual(0, result.messages.Count);
+        }
+    }
+}
+
