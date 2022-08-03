2ab0727cb156ba2c8703e15f589941d8e0ba086f
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-08-03 15:36:40 +0200

    +ADD: WorkingSetMasterName Tests +ADD: DeviceClassList

--- isoxml_dotnet_core/src/WSM.cs
+++ isoxml_dotnet_core/src/WSM.cs
@@ -4,12 +4,42 @@ using System.Text;
 
 namespace Dev4ag
 {
+    public enum DeviceClass
+    {
+        NonSpecificSystem,
+        Tractor,
+        PrimarySoilTillage,
+        SecondarySoilTillage,
+        PlantersSeeders_,
+        Fertilizer,
+        Sprayers,
+        Harvesters,
+        RootHarvester,
+        ForageHarvester,
+        Irrigation,
+        TransportTrailers_,
+        FarmyardWork,
+        PoweredAuxilary_Units,
+        SpecialCrops,
+        MunicipalWork,
+        SensorSystem,
+        ReservedForFutureAssignment,
+        TimberHarvesters,
+        Forwarders,
+        TimberLoaders,
+        TimberProcessingMachines,
+        Mulchers,
+        UtilityVehicles,
+        FeederMixer,
+        SlurryApplicators,
+    }
+
     public class WSM
     {
         static private String[] letters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
         public bool selfConfigurable;
         public int industryGroup;
-        public int deviceClass;
+        public DeviceClass deviceClass;
         public int deviceClassInstance;
         public int manufacturerCode;
         public int function;
@@ -30,7 +60,7 @@ namespace Dev4ag
             array[3] = (byte)((this.manufacturerCode) >> 3 & 0xFF);
             array[4] = (byte)(ecuInstance + (functionInstance << 3));
             array[5] = (byte)(function);
-            array[6] = (byte)(deviceClass << 1);
+            array[6] = (byte)((int)deviceClass << 1);
             array[7] = (byte)(industryGroup << 4);
             array[7] += (byte)((selfConfigurable ? 1 : 0) << 7);
             array[7] += (byte)(deviceClassInstance);
@@ -52,7 +82,7 @@ namespace Dev4ag
         {
             this.selfConfigurable = ((array[7] & 0xFF) & 128) == 128;
             this.industryGroup = ((array[7] & 0xFF) >> 4) & 0x7;
-            this.deviceClass = ((array[6] & 0xFF) >> 1);
+            this.deviceClass = (DeviceClass)((array[6] & 0xFF) >> 1);
             this.deviceClassInstance = ((array[7] & 0xFF) & 0xF);
             this.manufacturerCode = (((array[3] & 0xFF) << 3) | ((array[2] & 0xFF) >> 5));
             this.function = ((array[5] & 0xFF));
@@ -74,8 +104,8 @@ namespace Dev4ag
 
             for (int a = 0; a < 8; a++)
             {
-                String letter1 = input.Substring(a * 2, a * 2 + 1);
-                String letter2 = input.Substring(a * 2 + 1, a * 2 + 2);
+                String letter1 = input.Substring(a * 2,1);
+                String letter2 = input.Substring(a * 2 + 1, 1);
                 for (byte b = 0; b < letters.Length; b++)
                 {
                     if (letter1 == letters[b])
