9d215dd69c6879046424e16809967d06ed81fc34
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-11-22 12:15:30 +0100

    Add VersionInfo

--- Dev4Agriculture.ISO11783.ISOXML/Constants.cs
+++ Dev4Agriculture.ISO11783.ISOXML/Constants.cs
@@ -4,6 +4,6 @@
     {
         //TODO It's more save to move this Element to a common class. It's found in Generator and DotnetCore Library
         public static string ISOXMLClassName = "Dev4Agriculture.ISO11783.ISOXML";
-        public static string Version = "V0.7.4.3";//Currently unused; Just for the commit
+        public static string Version = "V0.7.4.4";//Currently unused; Just for the commit
     }
 }
