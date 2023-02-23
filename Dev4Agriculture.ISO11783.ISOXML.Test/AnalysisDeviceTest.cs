1c894d471ac75f05e5728ef9f9186be05c372b8f
Frank-Wiebeler <frank.wiebeler@dev4Agriculture.de>
2023-02-23 20:34:10 +0100

    -FIX: Adjust Linq-Statements +ADD: TestCases for findDeviceElements +UPD: Update Version Number

---
+++ Dev4Agriculture.ISO11783.ISOXML.Test/AnalysisDeviceTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dev4Agriculture.ISO11783.ISOXML.Analysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev4Agriculture.ISO11783.ISOXML.Test;
+[TestClass]
+public class AnalysisDeviceTest
+{
+    [TestMethod]
+    public void CanFindDPDs()
+    {
+        var isoxml = ISOXML.Load("testdata/TimeLogs/ValidTimeLogs");
+
+        var Analysis = new ISODeviceAnalysis(isoxml);
+        var task = isoxml.Data.Task[0];
+        var possibleDeviceElements = Analysis.FindDeviceElementsForDDI(task, 119);
+        Assert.AreEqual(possibleDeviceElements.Count, 1);
+        Assert.AreEqual(possibleDeviceElements[0].Type, DDIValueType.ProcessData);
+
+        possibleDeviceElements = Analysis.FindDeviceElementsForDDI(task, 179 /*ActualCulturalPractice*/);
+        Assert.AreEqual(possibleDeviceElements.Count, 1);
+        Assert.AreEqual(possibleDeviceElements[0].DeviceElement, "DET-2");
+
+    }
+
+
+}
--- Dev4Agriculture.ISO11783.ISOXML/Analysis/ISODeviceAnalysis.cs
+++ Dev4Agriculture.ISO11783.ISOXML/Analysis/ISODeviceAnalysis.cs
@@ -7,7 +7,7 @@ using Dev4Agriculture.ISO11783.ISOXML.TaskFile;
 namespace Dev4Agriculture.ISO11783.ISOXML.Analysis
 {
 
-    public enum DeviceValueType
+    public enum DDIValueType
     {
         None = 0,
         ProcessData = 1,
@@ -17,14 +17,14 @@ namespace Dev4Agriculture.ISO11783.ISOXML.Analysis
     public class TaskDDIEntry
     {
         public string DeviceElement;
-        public DeviceValueType Type;
+        public DDIValueType Type;
     }
 
 
     public class ISODeviceAnalysis
     {
         private ISOXML _isoxml;
-        private ISODeviceAnalysis(ISOXML isoxml)
+        public ISODeviceAnalysis(ISOXML isoxml)
         {
             _isoxml = isoxml;
         }
@@ -36,14 +36,14 @@ namespace Dev4Agriculture.ISO11783.ISOXML.Analysis
                         .Select(dlvEntry => new TaskDDIEntry()
                         {
                             DeviceElement = "DET" + dlvEntry.DeviceElement,
-                            Type = DeviceValueType.ProcessData
+                            Type = DDIValueType.ProcessData
                         }).ToList();
 
 
             //Find all assigned machines and in such, search for the properties.
             var properties = isoTask.DeviceAllocation.ToList()
-                //First Filter all DANs for those that are not planned)
-                .Where(deviceAllocation => deviceAllocation.AllocationStamp != null && deviceAllocation.AllocationStamp.Type != ISOType.Planned)
+                //First Filter all DANs for those that are not planned. Only filter if an AllocationStamp is available anyway)
+                .Where(deviceAllocation => deviceAllocation.AllocationStamp == null || deviceAllocation.AllocationStamp.Type != ISOType.Planned)
                 //Select all DeviceAllocations (DANs) and read the DET referencess
                 .Select(deviceAllocation => deviceAllocation.DeviceIdRef).ToList()
                 //So, here we have a list of DeviceElements (DETs) by their Reference ("DVC-1", "DVC5")
@@ -54,21 +54,18 @@ namespace Dev4Agriculture.ISO11783.ISOXML.Analysis
                 //Now, go through the list of DeviceProperties and check, if our DDI is available there
                 .SelectMany(device => device.DeviceProperty
                     //Find the ObjectID for our DPT
-                    .Select(dpt => (Utils.ConvertDDI(dpt.DevicePropertyDDI) == ddi) ? dpt.DeviceValuePresentationObjectId : null)
+                    .Where(dpt => (Utils.ConvertDDI(dpt.DevicePropertyDDI) == ddi))
+                    .Select(dpt => dpt.DevicePropertyObjectId)
                     //which is then linked in the DeviceObjectRelation(DOR) of a DET
-                    .SelectMany(dpt =>
+                    .SelectMany(dptObjectId =>
                             device.DeviceElement
-                           .Select(det => {
-                               //Now, if this DOR links our DPT from the machine in the Task, we found an entry to add
-                               if (det.DeviceObjectReference.Any(dor => dor.DeviceObjectId == dpt)) {
-                                   return new TaskDDIEntry()
+                            //Now, if this DOR links our DPT from the machine in the Task, we found an entry to add
+                            .Where( det => det.DeviceObjectReference.Any(dor => dor.DeviceObjectId == dptObjectId))
+                            .Select(det => new TaskDDIEntry()
                                    {
-                                       DeviceElement = "DET" + det.DeviceElementId,
-                                       Type = DeviceValueType.Property
-                                   };
-                               };
-                               return null;
-                           }).ToList()
+                                       DeviceElement = det.DeviceElementId,
+                                       Type = DDIValueType.Property
+                                    }).ToList()
                            ).ToList()
                     ).ToList();
 
