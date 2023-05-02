37fafa489bbf4de2d4335392df4b0d693e845e3c
Frank-Wiebeler <38665241+Frank-Wiebeler@users.noreply.github.com>
2023-05-02 12:27:42 +0200

    Bugfix/crash on invalid xml (#67)
    * -FIX: When an invalid XML was loaded, the Load-Function threw an exception
    
    * Add TestCase to check that reading an invalid ISOXML does not crash
    
    * Shorten ISOXML Just down to the error

---
+++ Dev4Agriculture.ISO11783.ISOXML.Test/ISOXMLLoadTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dev4Agriculture.ISO11783.ISOXML.Test;
+[TestClass]
+public class ISOXMLLoadTests
+{
+    [TestMethod]
+    public void LoadingInvalidISOXMLDoesNotCrash()
+    {
+        var isoxml = ISOXML.Load("./testdata/Structure/InvalidXML/");
+        Assert.AreEqual(isoxml.Data, null);
+    }
+}
