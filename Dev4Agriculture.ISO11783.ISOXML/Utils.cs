b8a80505bd8b4a86edc0edde427b847f88021ffe
NikitaChyzhykDev4agro <nikita.chyzhyk@dev4agriculture.de>
2023-05-25 15:42:55 +0300

    fixed after review

--- Dev4Agriculture.ISO11783.ISOXML/Utils.cs
+++ Dev4Agriculture.ISO11783.ISOXML/Utils.cs
@@ -76,6 +76,39 @@ namespace Dev4Agriculture.ISO11783.ISOXML
         {
             return BitConverter.ToString(data).Replace("-", "");
         }
+
+        public static CulturalPracticesType MapDeviceClassToPracticeType(DeviceClass className) => className switch
+        {
+            DeviceClass.NonSpecificSystem => CulturalPracticesType.Unknown,
+            DeviceClass.Tractor => CulturalPracticesType.Unknown,
+            DeviceClass.PrimarySoilTillage => CulturalPracticesType.Tillage,
+            DeviceClass.SecondarySoilTillage => CulturalPracticesType.Tillage,
+            DeviceClass.PlantersSeeders => CulturalPracticesType.SowingAndPlanting,
+            DeviceClass.Fertilizer => CulturalPracticesType.Fertilizing,
+            DeviceClass.Sprayers => CulturalPracticesType.CropProtection,
+            DeviceClass.Harvesters => CulturalPracticesType.Harvesting,
+            DeviceClass.RootHarvester => CulturalPracticesType.Harvesting,
+            DeviceClass.ForageHarvester => CulturalPracticesType.ForageHarvesting,
+            DeviceClass.Irrigation => CulturalPracticesType.Irrigation,
+            DeviceClass.TransportTrailers => CulturalPracticesType.Transport,
+            DeviceClass.FarmyardWork => CulturalPracticesType.Unknown,
+            DeviceClass.PoweredAuxilaryUnits => CulturalPracticesType.Unknown,
+            DeviceClass.SpecialCrops => CulturalPracticesType.Unknown,
+            DeviceClass.MunicipalWork => CulturalPracticesType.Unknown,
+            DeviceClass.UnDefined16 => CulturalPracticesType.Unknown,
+            DeviceClass.SensorSystem => CulturalPracticesType.Unknown,
+            DeviceClass.ReservedForFutureAssignment => CulturalPracticesType.Unknown,
+            DeviceClass.TimberHarvesters => CulturalPracticesType.Harvesting,
+            DeviceClass.Forwarders => CulturalPracticesType.Transport,
+            DeviceClass.TimberLoaders => CulturalPracticesType.Transport,
+            DeviceClass.TimberProcessingMachines => CulturalPracticesType.Unknown,
+            DeviceClass.Mulchers => CulturalPracticesType.Mulching,
+            DeviceClass.UtilityVehicles => CulturalPracticesType.Unknown,
+            DeviceClass.FeederMixer => CulturalPracticesType.Unknown,
+            DeviceClass.SlurryApplicators => CulturalPracticesType.SlurryManureApplication,
+            DeviceClass.Reserved => CulturalPracticesType.Unknown,
+            _ => CulturalPracticesType.Unknown
+        };
     }
 
     public sealed class StringWriterWithEncoding : StringWriter
