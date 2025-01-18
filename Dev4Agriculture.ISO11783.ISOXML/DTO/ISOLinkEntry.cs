3a649ddb5fa53d00f1b461cab1577ca060f3f40c
NikitaChyzhykDev4agro <127326880+NikitaChyzhykDev4agro@users.noreply.github.com>
2025-01-18 02:43:29 +0200

    ADD: ManufacturerGLN and LinkGroupNamespace to ISOLinkEntry (#110)
    * ADD: ManufacturerGLN and LinkGroupNamespace to ISOLinkEntry
    
    * Adjusted version
    
    * Update Pipeline to ensure all dependencies are loaded correctly (#111) (#112)
    
    * Update Pipeline to ensure all dependencies are loaded correctly
    
    * Try a different version
    
    * Aand another missing Lib is postinstalled
    
    * Split the LinuxPipeline into old and new and add more up to date Frameworks
    
    * Old versions build now; cheching how to build the new ones
    
    * -FIX: When exporting a Grid, non-existing paths were not created
    =OPT: We update the .net-Version in the tests to have compatible testruns (hopefully; will find out in the pipeline)
    
    * Check installed version before tests.
    
    * Targeting older version again
    
    * Adding Global.json for a Test of Framework compatibilities
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Updated test project
    
    ---------
    
    Co-authored-by: NikitaChyzhykDev4agro <127326880+NikitaChyzhykDev4agro@users.noreply.github.com>
    Co-authored-by: onixionR <onixionn@gmail.com>
    
    * Added a test case
    
    ---------
    
    Co-authored-by: onixionR <onixionn@gmail.com>
    Co-authored-by: Frank-Wiebeler <38665241+Frank-Wiebeler@users.noreply.github.com>

--- Dev4Agriculture.ISO11783.ISOXML/DTO/ISOLinkEntry.cs
+++ Dev4Agriculture.ISO11783.ISOXML/DTO/ISOLinkEntry.cs
@@ -8,6 +8,8 @@ namespace Dev4Agriculture.ISO11783.ISOXML.DTO
         public string Id { get; set; }
         public ISOLinkGroupType type { get; set; }
         public string Designator { get; set; }
+        public string LinkGroupNamespace { get;set; }
+        public string ManufacturerGLN { get; set; }
 
     }
 }
