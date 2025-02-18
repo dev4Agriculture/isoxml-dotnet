f2673f6999fe80ee37f1b2a03422453a9189db18
Frank-Wiebeler <frank.wiebeler@dev4Agriculture.de>
2025-02-18 08:38:57 +0100

    +ADD: VersionUpdate

--- Dev4Agriculture.ISO11783.ISOXML/Constants.cs
+++ Dev4Agriculture.ISO11783.ISOXML/Constants.cs
@@ -4,7 +4,7 @@
     {
         //TODO It's more save to move this Element to a common class. It's found in Generator and DotnetCore Library
         public static string ISOXMLClassName = "Dev4Agriculture.ISO11783.ISOXML";
-        public static string Version = "V0.22.1.3";//Currently unused; Just for the commit
+        public static string Version = "V0.23.1.1";//Currently unused; Just for the commit
         public static string Author = "Frank Wiebeler, dev4Agriculture";
         public const int TLG_VALUE_FOR_NO_VALUE = int.MinValue;
         public const double EarthRadiusInMeters = 6378137;
